798ceceaf10a21528e90c61c69ec2b4071a94375
Abhishek Pandey <rishabpandey333@gmail.com>
2022-02-23 20:31:52 +0530

    first commit

---
+++ resources/views/internal.blade.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
+    <title>Search</title>
+  </head>
+  <body>
+
+    <div class="container">
+        <div class="row">
+                <div class="col-md-12 search-list-page">
+                    <div class="search-div">
+                      <h1 class="mb-3 text-center">LOGO</h1>
+                        <div class="card">
+                          <div class="card-header bg-white font-weight-bold">
+                            Q1. Lorem ipsum dolor sit amet?
+                          </div>
+                          <div class="card-body">
+                            <div class="progress">
+                              <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width:75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="card">
+                          <div class="card-header bg-white font-weight-bold">
+                            Q2. Almost before we knew it, we had left the ground
+                          </div>
+                          <div class="card-body">
+                            <div class="row">
+                              <div class="col-md-3">
+                                <div class="form-check w-100 form-check-inline" >
+                                  <input class="form-check-input" type="radio" name="subServiceRadio" id="" value="">
+                                  <label class="form-check-label font-weight-bold w-100" for="">A</label>
+                                </div>
+                              </div>
+                        
+                              <div class="col-md-3">
+                                <div class="form-check w-100 form-check-inline" >
+                                  <input class="form-check-input" type="radio" name="subServiceRadio" id="" value="">
+                                  <label class="form-check-label font-weight-bold w-100" for="">B</label>
+                                </div>
+                              </div>
+                        
+                              <div class="col-md-3">
+                                <div class="form-check w-100 form-check-inline" >
+                                  <input class="form-check-input" type="radio" name="subServiceRadio" id="" value="">
+                                  <label class="form-check-label font-weight-bold w-100" for="">C</label>
+                                </div>
+                              </div>
+                        
+                              <div class="col-md-3">
+                                <div class="form-check w-100 form-check-inline" >
+                                  <input class="form-check-input" type="radio" name="subServiceRadio" id="" value="">
+                                  <label class="form-check-label font-weight-bold w-100" for="">D</label>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+                </div>
+              </div>
+        </div>
+    </div>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
+    <title>Search</title>
+  </head>
+  <body class="">
+<!-- <img src="https://img.freepik.com/free-vector/geometrical-abstract-background_53876-91649.jpg?t=st=1645162986~exp=1645163586~hmac=0cd2c0ab286ffb7666665eb962cb18cb2418c5243dbd3b573d9916581222ec4f&w=996" class="img-back" />
+<img src="https://img.freepik.com/free-vector/geometrical-abstract-background_53876-91649.jpg?t=st=1645162986~exp=1645163586~hmac=0cd2c0ab286ffb7666665eb962cb18cb2418c5243dbd3b573d9916581222ec4f&w=996" class="img-back-right" /> -->
+    <div class="container">
+        <div class="row">
+                <div class="col-md-6 m-auto search-page">
+                    <h1 class="text-center mb-3">LOGO</h1>
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search here" name=""   />
+                            <a href="search-list.html" type="" class="search-fa btn btn-default"><i class="far fa-search "></i></a>
+                        </div>
+                    </form>
+                </div>
+        </div>
+    </div>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
